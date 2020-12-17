1cd0ffe52576a92f1548f54836949949250bc5b1
951773148@qq.com <baobeiwoaini1314>
2020-12-17 13:30:56 +0800

    农场

--- src/pages/farmer/list/index.js
+++ src/pages/farmer/list/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { Input, Select, Button, DatePicker, Table, Space, Card, Divider, Typography, Drawer, Descriptions, Image } from 'antd';
+import { Input, Select, Button, DatePicker, Table, Space, Card, Divider, Typography, Drawer, Descriptions, Image, message } from 'antd';
 import { SearchOutlined, UserAddOutlined } from '@ant-design/icons';
+import httpUtils from '../../../utils/request'
 
 const { Option } = Select;
 const { RangePicker } = DatePicker;
@@ -19,100 +20,79 @@ const blockStyle = {
 
 const FarmerList = () => {
 
-    const [data, setData] = useState([])
-    const [pagination, setPagination] = useState({
-        current: 1,
-        pageSize: 10,
-        total: 200
-    })
+    const [farmerList, setFarmerList] = useState({})
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        setLoading(true)
-        setTimeout(() => {
-            let data = [
-                {
-                    id: 1,
-                    name: "张三",
-                    gender: 1,
-                    email: '1@123'
-                },
-                {
-                    id: 2,
-                    name: "张三02",
-                    gender: 1,
-                    email: '1@123'
-                },
-                {
-                    id: 3,
-                    name: "张三03",
-                    gender: 2,
-                    email: '1@123'
-                },
-                {
-                    id: 4,
-                    name: "张三04张三04张三04",
-                    gender: 2,
-                    email: '1@123'
-                },
-                {
-                    id: 5,
-                    name: "张三05",
-                    gender: 1,
-                    email: '1@123'
-                },
-                {
-                    id: 6,
-                    name: "张三06",
-                    gender: 1,
-                    email: '1@123'
-                }
-            ]
-            setData(data)
-            setLoading(false)
-        }, 2000)
+        getFarmerList()
     }, [])
+
+    const getFarmerList = async (pagination = {pageSize: 10, current: 1}) => {
+        let params = {
+            pageNum: pagination.current,
+            pageSize: pagination.pageSize
+        }
+        setLoading(true)
+        let resp = await httpUtils.get('/admin/farm/list', params)
+        setFarmerList(resp)
+        setLoading(false)
+    }   
 
     const columns = [
         {
             title: '序号',
-            dataIndex: 'name',
+            dataIndex: 'id',
             align: 'center',
         },
         {
             title: '农户姓名',
-            dataIndex: 'name',
+            dataIndex: 'farmName',
             align: 'center',
         },
         {
             title: '类别',
-            dataIndex: 'name',
+            dataIndex: 'farmType',
             align: 'center',
+            render: farmType =>  farmType == 1 ? '个人': '企业'
         },
         {
             title: '联系人',
-            dataIndex: 'name',
+            dataIndex: 'contactName',
             align: 'center',
         },
         {
             title: '地址',
-            dataIndex: 'name',
             align: 'center',
+            render: record => (record.provinceName || '') + (record.cityName || '') + (record.countyName || '') + (record.address || '')
         },
         {
             title: '手机号码',
-            dataIndex: 'name',
+            dataIndex: 'contactPhone',
             align: 'center',
         },
         {
             title: '合作状态',
-            dataIndex: 'name',
+            dataIndex: 'authStatus',
             align: 'center',
+            render: authStatus => {
+                if (authStatus == 1) {
+                    return '待审核'
+                } else if (authStatus == 2) {
+                    return '合作中'
+                } else if (authStatus == 3) {
+                    return '暂停合作'
+                } else if (authStatus == 4) {
+                    return '审核未通过'
+                } else {
+                    return ''
+                }
+            }
         },
         {
             title: '微信认证',
-            dataIndex: 'name',
+            dataIndex: 'wechatAuthStatus',
             align: 'center',
+            render: wechatAuthStatus => wechatAuthStatus == 1 ? '未认证': '已认证'
         },
         {
             title: '操作',
@@ -121,10 +101,16 @@ const FarmerList = () => {
             width: 300,
             render: (text, record) => (
                 <Space split={<Divider type="vertical" />}>
-                    <Typography.Link onClick={() => handleFarmerDetailDrawerOpen(record)}>去审核</Typography.Link>
-                    <Typography.Link>继续合作</Typography.Link>
-                    <Typography.Link type="danger">暂停合作</Typography.Link>
-                    <Typography.Link>详情</Typography.Link>
+                    {
+                        (record.authStatus == 1 || record.authStatus == 4) && <Typography.Link onClick={() => handleFarmerDetailDrawerOpen(record)}>去审核</Typography.Link>
+                    }
+                    {
+                        record.authStatus == 2 && <Typography.Link type="danger" onClick={() => pause(record.id)}>暂停合作</Typography.Link>
+                    }
+                    {
+                        record.authStatus == 3 && <Typography.Link onClick={() => goOn(record.id)}>继续合作</Typography.Link>
+                    }
+                    <Typography.Link onClick={() => handleFarmerDetailDrawerOpen(record)}>详情</Typography.Link>
                 </Space>
             )
         }
@@ -155,26 +141,68 @@ const FarmerList = () => {
     }
 
     //审核通过
-    const handleFarmerDetailDrawerApply = () => {
+    const handleFarmerDetailDrawerApply = async () => {
+        let data = {
+            farmId: farmerDetailDrawerData.id,
+            authStatus: 2
+        }
+        setFarmerDetailDrawerApplyLoading(true)
+        await httpUtils.post('/admin/farm/auth/update', data)
+        message.success('操作成功')
+        handleFarmerDetailDrawerClose()
+        getFarmerList()
+    }
 
+    //继续合作
+    const goOn = async id => {
+        let data = {
+            farmId: id,
+            authStatus: 2
+        }
+        await httpUtils.post('/admin/farm/auth/update', data)
+        message.success('操作成功')
+        getFarmerList()
+    }
+
+    //暂停合作
+    const pause = async id => {
+        let data = {
+            farmId: id,
+            authStatus: 3
+        }
+        await httpUtils.post('/admin/farm/auth/update', data)
+        message.success('操作成功')
+        getFarmerList()
     }
 
     //拒绝通过
-    const handleFarmerDetailDrawerRefuse = () => {
-
+    const handleFarmerDetailDrawerRefuse = async () => {
+        let data = {
+            farmId: farmerDetailDrawerData.id,
+            authStatus: 4
+        }
+        setFarmerDetailDrawerRefuseLoading(true)
+        await httpUtils.post('/admin/farm/auth/update', data)
+        message.success('操作成功')
+        handleFarmerDetailDrawerClose()
+        getFarmerList()
     }
 
     return (
         <div>
             <div style={blockStyle}>
                 <Table
+                    size="small"
                     bordered={true}
                     style={{marginTop: 12}}
                     columns={columns}
                     rowKey={record => record.id}
-                    dataSource={data}
-                    pagination={pagination}
+                    dataSource={farmerList.dataList}
+                    pagination={{
+                        total: farmerList.totalCount
+                    }}
                     loading={loading}
+                    onChange={async (pagination, filters, sorter) => getFarmerList(pagination)}
                 />
                 <Drawer
                     title={farmerDetailDrawerProps.title + "的农户资料"}
@@ -199,14 +227,17 @@ const FarmerList = () => {
                     }
                 >
                     <Descriptions bordered size="small">
-                        <Descriptions.Item label="类别" span={3}>企业</Descriptions.Item>
-                        <Descriptions.Item label="农场/供应商名称" span={3}>xxxxxx农场</Descriptions.Item>
-                        <Descriptions.Item label="农场管理员手机号" span={3}>18269950607</Descriptions.Item>
-                        <Descriptions.Item label="身份证号" span={3}>413026199212276543</Descriptions.Item>
-                        <Descriptions.Item label="土地测量面积" span={3}>300m2</Descriptions.Item>
+                        <Descriptions.Item label="类别" span={3}>{farmerDetailDrawerData.farmType == 1 ? '个人' : '企业'}</Descriptions.Item>
+                        <Descriptions.Item label="农场/供应商名称" span={3}>{farmerDetailDrawerData.farmName}</Descriptions.Item>
+                        <Descriptions.Item label="农场管理员手机号" span={3}>{farmerDetailDrawerData.contactPhone}</Descriptions.Item>
+                        <Descriptions.Item label="身份证号" span={3}>{farmerDetailDrawerData.farmName}</Descriptions.Item>
+                        <Descriptions.Item label="土地测量面积" span={3}>{farmerDetailDrawerData.square}</Descriptions.Item>
                         <Descriptions.Item label="土地测量面积图片" span={3}>
-                            <Image width={160} src={'https://test-zmy.oss-cn-hangzhou.aliyuncs.com/fa65e131ddee409e9f0763752c93d9e4-image-b8cc4986-8cbd-43e3-820b-3d5a77e43297.jpg'} />
-                            <Image width={160} src={'https://test-zmy.oss-cn-hangzhou.aliyuncs.com/fa65e131ddee409e9f0763752c93d9e4-image-b8cc4986-8cbd-43e3-820b-3d5a77e43297.jpg'} />
+                            {
+                                farmerDetailDrawerData.farmPics && farmerDetailDrawerData.farmPics.map((item, index) => (
+                                    <Image width={160} key={index} src={item} />
+                                ))
+                            }
                         </Descriptions.Item>
                     </Descriptions>
                 </Drawer>
